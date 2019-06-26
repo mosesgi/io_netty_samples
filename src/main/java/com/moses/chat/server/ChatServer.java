a3cb5b0d966801fd3308bb5773011170ddc26542
Moses Ji <jimuchen@163.com>
2019-06-26 14:45:58 +0800

    .

--- src/main/java/com/moses/chat/server/ChatServer.java
+++ src/main/java/com/moses/chat/server/ChatServer.java
@@ -33,31 +33,31 @@ public class ChatServer {
 		try {
 			ServerBootstrap server = new ServerBootstrap();
 			server.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-					.option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChannelInitializer<SocketChannel>() {
+			.option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChannelInitializer<SocketChannel>() {
 
-						@Override
-						protected void initChannel(SocketChannel ch) throws Exception {
-							ChannelPipeline pipeline = ch.pipeline();
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ChannelPipeline pipeline = ch.pipeline();
 
-							//用于java terminal
-							pipeline.addLast(new IMDecoder());
-							pipeline.addLast(new IMEncoder());
-							pipeline.addLast(new SocketHandler());
-							
-							//http requests
-		            		pipeline.addLast(new HttpServerCodec());
-		            		//主要是将同一个http请求或响应的多个消息对象变成一个 fullHttpRequest完整的消息对象
-		            		pipeline.addLast(new HttpObjectAggregator(64 * 1024));
-		            		//主要用于处理大数据流,比如一个1G大小的文件如果你直接传输肯定会撑暴jvm内存的 ,加上这个handler我们就不用考虑这个问题了
-		            		pipeline.addLast(new ChunkedWriteHandler());
-		            		pipeline.addLast(new HttpHandler());
-							
-		            		//webSocket requests
-		            		pipeline.addLast(new WebSocketServerProtocolHandler("/im"));
-		            		pipeline.addLast(new WebSocketHandler());
-						}
+					//用于java terminal
+					pipeline.addLast(new IMDecoder());
+					pipeline.addLast(new IMEncoder());
+					pipeline.addLast(new SocketHandler());
+					
+					//http requests
+            		pipeline.addLast(new HttpServerCodec());
+            		//主要是将同一个http请求或响应的多个消息对象变成一个 fullHttpRequest完整的消息对象
+            		pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+            		//主要用于处理大数据流,比如一个1G大小的文件如果你直接传输肯定会撑暴jvm内存的 ,加上这个handler我们就不用考虑这个问题了
+            		pipeline.addLast(new ChunkedWriteHandler());
+            		pipeline.addLast(new HttpHandler());
+					
+            		//webSocket requests
+            		pipeline.addLast(new WebSocketServerProtocolHandler("/im"));
+            		pipeline.addLast(new WebSocketHandler());
+				}
 
-					});
+			});
 			ChannelFuture future = server.bind(this.port).sync();
 			LOG.info("server started, listening on port: " + this.port);
 			future.channel().closeFuture().sync();
